0a182494675c440db6df3060676000238aad8c3f
fajaryudha23@gmail.com <fajaryudha23@gmail.com>
2020-11-13 16:22:44 +0700

    Change

--- lib/authentication_bloc/bloc/authentication_bloc_dart_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'authentication_bloc_dart_bloc.dart';
-
-abstract class AuthenticationBlocDartEvent extends Equatable {
-  const AuthenticationBlocDartEvent();
-
-  @override
-  List<Object> get props => [];
-}
